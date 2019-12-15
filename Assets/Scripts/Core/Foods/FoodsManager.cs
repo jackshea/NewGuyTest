58c889af51226b3bb890adf3e01abb5ad0b623bd
谢在奎 <jackshea@163.com>
2019-12-16 00:41:09 +0800

    地面处理

--- Assets/Scripts/Core/Foods/FoodsManager.cs
+++ Assets/Scripts/Core/Foods/FoodsManager.cs
@@ -5,37 +5,35 @@ using Random = UnityEngine.Random;
 namespace Core.Foods
 {
     /// <summary>
-    /// 管理场景中的食物
+    ///     管理场景中的食物
     /// </summary>
     public class FoodsManager
     {
-        public event Action<Food> OnAddFood;
-        public event Action<Food> OnRemoveFood;
-
-        public List<Food> Foods { get; private set; } = new List<Food>();
-
-        public static FoodsManager Instance { get; } = new FoodsManager();
-
         private int SpawnFoodId;
 
         private FoodsManager()
         {
-
         }
+
+        public List<Food> Foods { get; } = new List<Food>();
+
+        public static FoodsManager Instance { get; } = new FoodsManager();
+        public event Action<Food> OnAddFood;
+        public event Action<Food> OnRemoveFood;
 
         public void Init()
         {
             // 生成初始食物
             var foodTypeLength = Enum.GetValues(typeof(FoodType)).Length;
-            for (int i = 0; i < 30; i++)
+            for (var i = 0; i < 30; i++)
             {
-                var type = (FoodType)Random.Range(0, foodTypeLength);
+                var type = (FoodType) Random.Range(0, foodTypeLength);
                 Spawn(type);
             }
         }
 
         /// <summary>
-        /// 生成食物
+        ///     生成食物
         /// </summary>
         public Food Spawn(FoodType type)
         {
@@ -45,15 +43,12 @@ namespace Core.Foods
         }
 
         /// <summary>
-        /// 添加食物
+        ///     添加食物
         /// </summary>
         public bool AddFood(Food food)
         {
             var existFood = Foods.Find(p => p.Id == food.Id);
-            if (existFood != null)
-            {
-                return false;
-            }
+            if (existFood != null) return false;
 
             Foods.Add(food);
             OnAddFood?.Invoke(food);
@@ -61,23 +56,17 @@ namespace Core.Foods
         }
 
         /// <summary>
-        /// 减少食物
+        ///     减少食物
         /// </summary>
         /// <param name="foodId">食物Id</param>
         /// <returns></returns>
         public bool RemoveFood(int foodId)
         {
             var food = Foods.Find(p => p.Id == foodId);
-            if (food == null)
-            {
-                return false;
-            }
+            if (food == null) return false;
 
             var success = Foods.Remove(food);
-            if (success)
-            {
-                OnRemoveFood?.Invoke(food);
-            }
+            if (success) OnRemoveFood?.Invoke(food);
 
             return success;
         }
